5421ed2f9b24cf61eb2b07b58ac2d65c6352280b
Vicki Avola <vavola@comunyt.co>
2023-11-25 08:56:04 +0100

    Instalacion de emotion

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { Bars3Icon, BugAntIcon } from "@heroicons/react/24/outline";
+import { Bars3Icon } from "@heroicons/react/24/outline";
 import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaffold-eth";
 import { useOutsideClick } from "~~/hooks/scaffold-eth";
 
@@ -16,8 +16,7 @@ export const menuLinks: HeaderMenuLink[] = [
   //Aquí se cambia los textos del menú
   {
     label: "Inicio",
-    href: "/",
-    icon: <BugAntIcon className="h-4 w-4" />,
+    href: "/inicio",
   },
   {
     label: "¿Quiénes somos?",
