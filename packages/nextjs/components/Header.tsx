98b8ff7de96e60ec2181fbf73f661c256eb97a06
Vicki Avola <vickiavola7@gmail.com>
2023-12-02 09:24:21 +0100

    Solo componentes necesarios en el Header

--- packages/nextjs/components/Header.tsx
+++ packages/nextjs/components/Header.tsx
@@ -10,47 +10,53 @@ import { FaucetButton, RainbowKitCustomConnectButton } from "~~/components/scaff
 export const Header = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-md navbar-light">
-        <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbar-toggler"
-            aria-controls="navbarTogglerDemo01"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbar-toggler">
-            <a className="navbar-brand" href="#">
-              <img src="/marca_pool.svg" className="rounded float-start" alt="logo" width="70" />
-            </a>
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Perfil
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  Comunidad
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">
-                  FAQ
-                </a>
-              </li>
-            </ul>
-            <div className="">
-              <RainbowKitCustomConnectButton />
-              <FaucetButton />
-            </div>
+      <div className="container-fluid align-items-center bg-(var(--green400)">
+        <div className="d-flex">
+          <div className="col-auto">
+            <nav className="navbar navbar-expand-md navbar-light">
+              <div className="container-fluid">
+                <button
+                  className="navbar-toggler"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target="#navbar-toggler"
+                  aria-controls="navbarTogglerDemo01"
+                  aria-expanded="false"
+                  aria-label="Toggle navigation"
+                >
+                  <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbar-toggler">
+                  <a className="navbar-brand" href="#">
+                    <img src="/marca_pool.svg" className="rounded float-start" alt="logo" width="70" />
+                  </a>
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">
+                        Perfil
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">
+                        Comunidad
+                      </a>
+                    </li>
+                    <li className="nav-item">
+                      <a className="nav-link" href="#">
+                        FAQ
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
           </div>
+          <div className="col-8"></div>
+
+          <RainbowKitCustomConnectButton />
+          <FaucetButton />
         </div>
-      </nav>
+      </div>
     </>
   );
 };
